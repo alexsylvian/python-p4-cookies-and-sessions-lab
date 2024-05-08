9f6ccfcf16a1b087fd82ce0fc98c2bc9aad78a96
alexsylvian <lazeratus1@gmail.com>
2024-05-07 22:00:08 -0400

    pages added

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -17,18 +16,43 @@ db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
-
-@app.route('/articles')
-def index_articles():
-
-    pass
+    session.clear()
+    return jsonify({'message': 'Successfully cleared session data.'}), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    print("NU" + '1')
+    # print(session['page_views'])
+    session['page_views'] = session.get('page_views', 0)
+    love = int(session['page_views'])
+    print(love)
+    session['page_views'] += 1
+    
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+    
+    article = Article.query.get_or_404(id)
+    return jsonify({
+        'id': article.id,
+        'title': article.title,
+        'content': article.content,
+        'author': article.author,
+        'preview': article.content[:100],  # Example preview
+        'minutes_to_read': article.minutes_to_read,  # Example minutes_to_read
+        'date': article.date.strftime('%Y-%m-%d')  # Example date formatting
+    })
 
-    pass
+@app.route('/articles')
+def index_articles():
+    articles = Article.query.all()
+    article_list = []
+    for article in articles:
+        article_list.append({
+            'id': article.id,
+            'title': article.title,
+            'author': article.author
+        })
+    return jsonify(article_list)
 
 if __name__ == '__main__':
     app.run(port=5555)
